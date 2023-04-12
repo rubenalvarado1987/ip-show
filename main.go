45a24be654b97d39f832bf180fa16fb87e1c83be
Ruben Alvarado <ruben.alvarado0@walmart.com>
2023-04-12 17:31:52 -0400

    changes in fly

--- main.go
+++ main.go
@@ -23,8 +23,6 @@ func main() {
 	router := httprouter.New()
 	router.GET("/", api.GetIP)
 
-	router.GET("/healthz", "ok")
-
 	// Setup 404 / 405 handlers.
 	router.NotFound = http.HandlerFunc(api.NotFound)
 	router.MethodNotAllowed = http.HandlerFunc(api.MethodNotAllowed)
